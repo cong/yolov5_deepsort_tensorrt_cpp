131a1316ff5df97f27b1bfbadd47d931cc612cb5
cong <iwangcong@outlook.com>
2021-10-15 15:03:00 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,7 +13,6 @@ import time
 
 if __name__ == '__main__':
 
-    # video_path = './video/test.mp4'
     yolo_engine = './yolov5/yolov5s.engine'
     sort_engine = './deepsort/deepsort.engine'
     capture = cv2.VideoCapture(0)
@@ -27,10 +26,6 @@ if __name__ == '__main__':
             print('No image input!')
             break
 
-        # t1 = time.time()
-        # bboxes = yolo_deepsort_test.detect(img)
-        # track_bboxes = yolo_deepsort_test.detect_track(img)
-        # print(track_bboxes)
         t1 = time.time()
         frame = yolo_deepsort_test.detect_track_frame(img)
         t2 = time.time() - t1
